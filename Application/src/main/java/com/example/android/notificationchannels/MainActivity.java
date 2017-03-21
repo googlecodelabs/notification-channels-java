3d37ae2642dbcfe92f63770c9c5ac92f319125e0
google-automerger <automerger@google.com>
2017-03-21 10:30:11 -0700

    Initial Code

---
+++ Application/src/main/java/com/example/android/notificationchannels/MainActivity.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.notificationchannels;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+
+/** Display main screen for sample. Displays controls for sending test notifications. */
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int NOTIFICATION_FOLLOW = 1100;
+    private static final int NOTIFICATION_UNFOLLOW = 1101;
+    private static final int NOTIFICATION_DM_FRIEND = 1200;
+    private static final int NOTIFICATION_DM_COWORKER = 1201;
+
+    /*
+     * A view model for interacting with the UI elements.
+     */
+    private MainUi mUIModel;
+
+    /*
+     * A helper class for initializing notification channels and sending notifications.
+     */
+    private NotificationHelper mNotificationHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNotificationHelper = new NotificationHelper(this);
+        mUIModel = new MainUi(findViewById(R.id.activity_main));
+    }
+
+    /**
+     * Send activity notifications.
+     *
+     * @param id The ID of the notification to create
+     */
+    private void sendNotification(int id) {
+        Notification.Builder notificationBuilder = null;
+        switch (id) {
+            case NOTIFICATION_FOLLOW:
+                notificationBuilder =
+                        mNotificationHelper.getNotificationFollower(
+                                getString(R.string.follower_title_notification),
+                                getString(R.string.follower_added_notification_body,
+                                        mNotificationHelper.getRandomName()));
+                break;
+
+            case NOTIFICATION_UNFOLLOW:
+                notificationBuilder =
+                        mNotificationHelper.getNotificationFollower(
+                                getString(R.string.follower_title_notification),
+                                getString(R.string.follower_removed_notification_body,
+                                        mNotificationHelper.getRandomName()));
+                break;
+
+            case NOTIFICATION_DM_FRIEND:
+                notificationBuilder =
+                        mNotificationHelper.getNotificationDM(
+                                getString(R.string.direct_message_title_notification),
+                                getString(R.string.dm_friend_notification_body,
+                                        mNotificationHelper.getRandomName()));
+                break;
+
+            case NOTIFICATION_DM_COWORKER:
+                notificationBuilder =
+                        mNotificationHelper.getNotificationDM(
+                                getString(R.string.direct_message_title_notification),
+                                getString(R.string.dm_coworker_notification_body,
+                                        mNotificationHelper.getRandomName()));
+                break;
+        }
+        if (notificationBuilder != null) {
+            mNotificationHelper.notify(id, notificationBuilder);
+        }
+    }
+
+    /** Send Intent to load system Notification Settings for this app. */
+    private void goToNotificationSettings() {
+        Intent i = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+        i.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+        startActivity(i);
+    }
+
+    /**
+     * Send intent to load system Notification Settings UI for a particular channel.
+     *
+     * @param channel Name of channel to configure
+     */
+    private void goToNotificationChannelSettings(String channel) {
+        // Skeleton method to be completed later
+    }
+
+    /**
+     * View model for interacting with Activity UI elements. (Keeps core logic for sample separate.)
+     */
+    class MainUi implements View.OnClickListener {
+
+        private MainUi(View root) {
+
+            // Setup the buttons
+            (root.findViewById(R.id.birthday_follower_button)).setOnClickListener(this);
+            (root.findViewById(R.id.life_follower_button)).setOnClickListener(this);
+            (root.findViewById(R.id.follower_channel_settings_button)).setOnClickListener(this);
+            (root.findViewById(R.id.friend_dm_button)).setOnClickListener(this);
+            (root.findViewById(R.id.coworker_dm_button)).setOnClickListener(this);
+            (root.findViewById(R.id.dm_channel_settings_button)).setOnClickListener(this);
+            (root.findViewById(R.id.go_to_settings_button)).setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.birthday_follower_button:
+                    sendNotification(NOTIFICATION_FOLLOW);
+                    break;
+                case R.id.life_follower_button:
+                    sendNotification(NOTIFICATION_UNFOLLOW);
+                    break;
+                case R.id.follower_channel_settings_button:
+                    goToNotificationChannelSettings("");
+                    break;
+                case R.id.friend_dm_button:
+                    sendNotification(NOTIFICATION_DM_FRIEND);
+                    break;
+                case R.id.coworker_dm_button:
+                    sendNotification(NOTIFICATION_DM_COWORKER);
+                    break;
+                case R.id.dm_channel_settings_button:
+                    goToNotificationChannelSettings("");
+                    break;
+                case R.id.go_to_settings_button:
+                    goToNotificationSettings();
+                    break;
+                default:
+                    Log.e(TAG, getString(R.string.error_click));
+                    break;
+            }
+        }
+    }
+}
---
+++ Application/src/main/java/com/example/android/notificationchannels/NotificationHelper.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.notificationchannels;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Color;
+import java.util.Random;
+
+/** Helper class to manage notification channels, and create notifications. */
+class NotificationHelper extends ContextWrapper {
+    private NotificationManager mNotificationManager;
+    public static final String FOLLOWERS_CHANNEL = "follower";
+
+    /**
+     * Registers notification channels, which can be used later by individual notifications.
+     *
+     * @param context The application context
+     */
+    public NotificationHelper(Context context) {
+        super(context);
+
+        // Create the channel object with the unique ID FOLLOWERS_CHANNEL
+        NotificationChannel followersChannel =
+                new NotificationChannel(
+                        FOLLOWERS_CHANNEL,
+                        getString(R.string.notification_channel_followers),
+                        NotificationManager.IMPORTANCE_DEFAULT);
+
+        // Configure the channel's initial settings
+        followersChannel.setLightColor(Color.GREEN);
+        followersChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        // Submit the notification channel object to the notification manager
+        getNotificationManager().createNotificationChannel(followersChannel);
+
+    }
+
+    /**
+     * Get a follow/un-follow notification
+     *
+     * <p>Provide the builder rather than the notification it's self as useful for making
+     * notification changes.
+     *
+     * @param title the title of the notification
+     * @param body the body text for the notification
+     * @return A Notification.Builder configured with the selected channel and details
+     */
+    public Notification.Builder getNotificationFollower(String title, String body) {
+        return new Notification.Builder(getApplicationContext())
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(getSmallIcon())
+                .setAutoCancel(true);
+    }
+
+    /**
+     * Get a direct message notification
+     *
+     * <p>Provide the builder rather than the notification it's self as useful for making
+     * notification changes.
+     *
+     * @param title Title for notification.
+     * @param body Message for notification.
+     * @return A Notification.Builder configured with the selected channel and details
+     */
+    public Notification.Builder getNotificationDM(String title, String body) {
+        return new Notification.Builder(getApplicationContext())
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(getSmallIcon())
+                .setAutoCancel(true);
+    }
+
+    /**
+     * Create a PendingIntent for opening up the MainActivity when the notification is pressed
+     *
+     * @return A PendingIntent that opens the MainActivity
+     */
+    private PendingIntent getPendingIntent() {
+        Intent openMainIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(openMainIntent);
+        return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_ONE_SHOT);
+    }
+
+    /**
+     * Send a notification.
+     *
+     * @param id The ID of the notification
+     * @param notification The notification object
+     */
+    public void notify(int id, Notification.Builder notification) {
+        getNotificationManager().notify(id, notification.build());
+    }
+
+    /**
+     * Get the small icon for this app
+     *
+     * @return The small icon resource id
+     */
+    private int getSmallIcon() {
+        return android.R.drawable.stat_notify_chat;
+    }
+
+    /**
+     * Get the notification mNotificationManager.
+     *
+     * <p>Utility method as this helper works with it a lot.
+     *
+     * @return The system service NotificationManager
+     */
+    private NotificationManager getNotificationManager() {
+        if (mNotificationManager == null) {
+            mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return mNotificationManager;
+    }
+
+    /**
+     * Get a random name string from resources to add personalization to the notification
+     *
+     * @return A random name
+     */
+    public String getRandomName() {
+        String[] names = getApplicationContext().getResources().getStringArray(R.array.names_array);
+        return names[new Random().nextInt(names.length)];
+    }
+}
